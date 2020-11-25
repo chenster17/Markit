b64bf159ed92e63beaa83f7c071839d6470cda2a
Zhao Chen <Jchen1117@gmail.com>
2020-11-25 17:08:53 -0500

    communicating between parent and child component

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-mainpage',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mainpage.component.css']
 })
 export class MainpageComponent implements OnInit {
-
+  @Input() mainpage: string;
   constructor() { }
 
   ngOnInit(): void {
