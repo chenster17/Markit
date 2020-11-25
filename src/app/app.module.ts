5e0fa20e6339b6de1910782b07017f2959eab754
Zhao Chen <Jchen1117@gmail.com>
2020-11-25 16:51:03 -0500

    Added a Navbar and Mainpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
